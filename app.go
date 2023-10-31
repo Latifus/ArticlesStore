51defda372024a534beecc7fa1227635c1a326ad
Latifus <saitbaevlatif@gmail.com>
2023-10-31 02:40:35 +0100

    add registration func an drefactoring

--- app.go
+++ app.go
@@ -19,14 +19,22 @@ type Item struct {
 	Title        string
 	Description  string
 }
+
+type User struct {
+	ID       uint16
+	Username string
+	Email    string
+	Password string
+}
+
 var items = []Item{}
 
-func formatDate(date string) string  {
+func formatDate(date string) string {
 	t, err := time.Parse("2006-01-02 15:04:05", date)
-    if err != nil {
-        return "Invalid Date"
-    }
-    return t.Format("2006 January 02")
+	if err != nil {
+		return "Invalid Date"
+	}
+	return t.Format("2006 January 02")
 }
 
 func getHomePage(w http.ResponseWriter, r *http.Request) {
@@ -63,11 +71,80 @@ func getHomePage(w http.ResponseWriter, r *http.Request) {
 	temp.ExecuteTemplate(w, "index", items)
 }
 
+func addItem(w http.ResponseWriter, r *http.Request) {
+	temp, err := template.ParseFiles("front/add.html", "front/header.html", "front/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	temp.ExecuteTemplate(w, "create", nil)
+}
+
+func save(w http.ResponseWriter, r *http.Request) {
+
+	author := r.FormValue("author")
+	title := r.FormValue("title")
+	desc := r.FormValue("desc")
+	date := time.Now().Format("2006-01-02 15:04:05")
+
+	db, errr := sql.Open("mysql", "root:50151832l@tcp(127.0.0.1:3306)/project_db")
+	if errr != nil {
+		panic(errr)
+	}
+	defer db.Close()
+
+	insert, error := db.Query(fmt.Sprintf("INSERT INTO `items` (`authorName`, `creationDate`, `likes`, `title`, `description`) VALUES('%s', '%s', '%d', '%s', '%s')", author, date, 0, title, desc))
+	if error != nil {
+		panic(error)
+	}
+	defer insert.Close()
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func checkCriteria(criteria string) bool {
+	return criteria != "";
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+
+	username := r.FormValue("username")
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	temp, err := template.ParseFiles("front/register.html", "front/footer.html")
+	if err != nil {
+		panic(err)
+	}
+
+	db, errr := sql.Open("mysql", "root:50151832l@tcp(127.0.0.1:3306)/project_db")
+	if errr != nil {
+		panic(errr)
+	}
+	defer db.Close()
+
+	temp.ExecuteTemplate(w, "register", nil)
+
+	if checkCriteria(username) && checkCriteria(email) && checkCriteria(password) {
+		insert, error := db.Query(fmt.Sprintf("INSERT INTO `users` (`username`, `email`, `password`) VALUES('%s', '%s',  '%s')", username, email, password))
+		if error != nil {
+			panic(error)
+		}
+		defer insert.Close()
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+
+}	
+
 func main() {
 	rtr := mux.NewRouter()
 	http.Handle("/", rtr)
 
 	rtr.HandleFunc("/", getHomePage).Methods("GET")
+	rtr.HandleFunc("/add", addItem).Methods("GET")
+	rtr.HandleFunc("/save_article", save).Methods("POST")
+	rtr.HandleFunc("/register", register).Methods("GET")
 
 	http.ListenAndServe(":8181", nil)
 }
