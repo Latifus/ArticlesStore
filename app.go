8d47189611d89ed3ad21db8094bb92281c80a7fd
Latifus <saitbaevlatif@gmail.com>
2023-11-01 13:51:06 +0100

    add updates to app, user html

--- app.go
+++ app.go
@@ -25,24 +25,31 @@ type User struct {
 	Username string
 	Email    string
 	Password string
-	Token string
+	Token    string
 }
 type Exception struct {
 	Message string
 }
 
-func (u User) setToken(token string)  {
+var secretKey string = "ZrStAfUuqTM6eTuhacT9JCfUQp9QkHnZ"
+
+func (u *User) setToken(token string) {
 	u.Token = token
 }
 
-var secretKey string = "ZrStAfUuqTM6eTuhacT9JCfUQp9QkHnZ"
+func (u User) getToken() string {
+	return u.Token
+}
 
 func generateToken(userID int) string {
 	token := jwt.New(jwt.SigningMethodHS256)
 
 	claims := token.Claims.(jwt.MapClaims)
 	claims["user_id"] = userID
-	claims["exp"] = time.Now().Add(time.Hour * 24).Unix() // Время жизни токена, например, 24 часа
+	fmt.Println("saved id in claim")
+	fmt.Println(claims["user_id"])
+
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
 
 	tokenString, err := token.SignedString([]byte(secretKey))
 	if err != nil {
@@ -61,30 +68,47 @@ func formatDate(date string) string {
 }
 
 func checkToken(tokenString string) (jwt.MapClaims, error) {
-    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-        // Проверка метода подписи
-        if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-            return nil, fmt.Errorf("Invalid token signing method")
-        }
-        return []byte(secretKey), nil
-    })
-    if err != nil {
-        return nil, err
-    }
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Проверка метода подписи
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Invalid token signing method")
+		}
+		return []byte(secretKey), nil
+	})
+	if err != nil {
+		return nil, err
+	}
 
-    if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-        return claims, nil
-    }
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
 
-    return nil, fmt.Errorf("Invalid token")
+	return nil, fmt.Errorf("Invalid token")
 }
 
-func CheckAuthentication(r *http.Request) bool {
-	return true;
+func CheckAuthentication(tokenString string) bool {
+
+	if tokenString == "" {
+		return false
+	}
+
+	_, err := checkToken(tokenString)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	return true
 }
 
 func getHomePage(w http.ResponseWriter, r *http.Request) {
-	isAuthenticated := CheckAuthentication(r)
+	var isAuthenticated bool
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		isAuthenticated = false
+	} else {
+		token := cookie.Value
+		isAuthenticated = CheckAuthentication(token)
+	}
 
 	temp, err := template.ParseFiles("front/home.html", "front/header.html", "front/headerIfNonAuthorize.html", "front/footer.html")
 	if err != nil {
@@ -118,7 +142,6 @@ func getHomePage(w http.ResponseWriter, r *http.Request) {
 		if error != nil {
 			panic(error)
 		}
-
 		item.CreationDate = formatDate(item.CreationDate)
 
 		data.Items = append(data.Items, item)
@@ -207,7 +230,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	res, err := db.Query("SELECT `email`, `password` FROM `users`")
+	res, err := db.Query("SELECT `id`, `email`, `password` FROM `users`")
 	if err != nil {
 		panic(errr)
 	}
@@ -217,14 +240,24 @@ func login(w http.ResponseWriter, r *http.Request) {
 		found := false
 		for res.Next() {
 			var user User
-			err = res.Scan(&user.Email, &user.Password)
+			err = res.Scan(&user.ID, &user.Email, &user.Password)
 			if err != nil {
 				panic(err)
 			}
 
 			if user.Email == email && user.Password == password {
 				found = true
+				fmt.Println("user Id to send " + fmt.Sprint(user.ID))
 				user.setToken(generateToken(int(user.ID)))
+
+				fmt.Println("token: " + user.getToken())
+				cooke := http.Cookie{
+					Name:     "token",
+					Value:    user.getToken(),
+					Expires:  time.Now().Add(24 * time.Hour),
+					HttpOnly: true,
+				}
+				http.SetCookie(w, &cooke)
 				break
 			}
 		}
@@ -240,6 +273,53 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func verifyToken(token string) int {
+	claims, _ := checkToken(token)
+	fmt.Println("claims: ")
+	fmt.Println(claims)
+
+	fmt.Println("variable in claim id:")
+	fmt.Print(claims["user_id"])
+	fmt.Println("--------")
+
+	userID, _ := claims["user_id"].(float64)
+	fmt.Println("Id from claims: " + fmt.Sprint(userID))
+	return int(userID)
+}
+
+func checkUserPage(w http.ResponseWriter, r *http.Request) {
+	token, _ := r.Cookie("token")
+	fmt.Println(token)
+	userId := verifyToken(token.Value)
+	fmt.Println("ID: " + fmt.Sprint(userId))
+
+	temp, err := template.ParseFiles("front/user.html", "front/footer.html", "front/header.html")
+	if err != nil {
+		panic(err)
+	}
+
+	db, errr := sql.Open("mysql", "root:50151832l@tcp(127.0.0.1:3306)/project_db")
+	if errr != nil {
+		panic(errr)
+	}
+	defer db.Close()
+
+	res, exce := db.Query(fmt.Sprintf("SELECT * FROM `users` WHERE `id` = %d", userId))
+	if err != nil {
+		panic(exce)
+	}
+
+	var user User
+	for res.Next() {
+		err = res.Scan(&user.ID, &user.Username, &user.Email, &user.Password)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	temp.ExecuteTemplate(w, "user", user)
+}
+
 func main() {
 	rtr := mux.NewRouter()
 	http.Handle("/", rtr)
@@ -250,6 +330,7 @@ func main() {
 	rtr.HandleFunc("/register", getRegisterPage).Methods("GET")
 	rtr.HandleFunc("/save_user", register).Methods("POST")
 	rtr.HandleFunc("/login", login).Methods("GET")
+	rtr.HandleFunc("/user", checkUserPage).Methods("GET")
 
 	http.ListenAndServe(":8181", nil)
 }
